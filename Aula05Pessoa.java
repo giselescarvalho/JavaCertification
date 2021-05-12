27aab1933296c32d5a9ba781894cf477360cf349
unknown <giseledasilvac@gmail.com>
2021-05-12 20:02:37 -0300

    Chamadas de métodos nos objetos

---
+++ Aula05Pessoa.java
@@ -0,0 +1,12 @@
+class Aula05Pessoa {
+    String nome;
+    int idade;
+
+    public String getNome(){
+        return nome;
+    }
+    public void setNome(String nome, int idade){
+        this.nome = nome;
+        this.idade = idade;
+    }
+}
