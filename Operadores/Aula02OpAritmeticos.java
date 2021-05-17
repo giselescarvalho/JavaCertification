6e5500463f2acf18eac94725b4cfc9068400d253
unknown <giseledasilvac@gmail.com>
2021-05-17 20:53:03 -0300

    Operadores aritméticos

---
+++ Operadores/Aula02OpAritmeticos.java
@@ -0,0 +1,20 @@
+public class Aula02OpAritmeticos {
+    public static void main (String args[]){
+        byte dois = 2;
+        byte dez = 10;
+
+        int doze = dez + dois;
+        System.out.println(doze);
+        int oito = dez - dois;
+        int vinte = dez * dez;
+        int cinco = dez / dois;
+        int restoDaDivisaoCincoPorDois = 5%2;
+
+
+    }
+}
+/* operação com int devolve no mínimo um int
+operação com byte o mínimo que devolve é int byte dois = 2; byte dez = 10; int doze = dez + dois;
+operação com float e int devolve no mínimo float
+
+*/
