65219426b8b23db339812ecb002c58259444246b
unknown <giseledasilvac@gmail.com>
2021-05-17 21:00:18 -0300

    Operadores de comparação

---
+++ Operadores/Aula03OpComparacao.java
@@ -0,0 +1,29 @@
+public class Aula03OpComparacao {
+    public static void main(String args[]){
+        System.out.println(1 == 1); //true
+        System.out.println(1 != 1); //false
+        System.out.println(1 == 2); //false
+
+        System.out.println("_______");
+
+        System.out.println(1 >= 1);
+        System.out.println(2 <= 1);
+        System.out.println(1 > 1);
+        System.out.println(1 < 1);
+
+        System.out.println("_boolean_");
+
+        System.out.println(true == true); //true
+        System.out.println(true != true);//false
+
+        System.out.println("_______");
+
+        int idade = 25;
+        System.out.println(idade == 1); //false;
+        System.out.println(idade = 1);  //o valor 1 foi atribuido a idade
+        System.out.println("_______");
+
+
+
+    }
+}
