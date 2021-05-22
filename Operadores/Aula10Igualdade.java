7904d94e71acdcfa4c362db36a2e283da79f9593
unknown <giseledasilvac@gmail.com>
2021-05-22 13:40:14 -0300

    Igualdade

---
+++ Operadores/Aula10Igualdade.java
@@ -0,0 +1,8 @@
+public class Aula10Igualdade {
+    public static void main(String args[]) {
+    String ab = "a" + "b";
+    System.out.println(ab == "ab"); //concatenação de duas strings literais é um resultado literal
+
+
+    }
+}
